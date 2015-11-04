21175f8bc820d7bf92ecdb551604c0e8e62d9ba3
Tane Piper <piper.tane@gmail.com>
2015-11-04 14:01:31 +0000

    Fix lib folder issue

---
+++ src/server/plugins/url-handler/lib/stations.js
@@ -0,0 +1,116 @@
+'use strict';
+
+export default [
+  {
+    "code": "BSO",
+    "name": "Basildon"
+  },
+  {
+    "code": "BKG",
+    "name": "Barking"
+  },
+  {
+    "code": "BEF",
+    "name": "Benfleet"
+  },
+  {
+    "code": "CFH",
+    "name": "Chafford Hundred Lakeside"
+  },
+  {
+    "code": "CHW",
+    "name": "Chalkwell"
+  },
+  {
+    "code": "DDK",
+    "name": "Dagenham Dock"
+  },
+  {
+    "code": "ETL",
+    "name": "East Tilbury"
+  },
+  {
+    "code": "FST",
+    "name": "London Fenchurch Street"
+  },
+  {
+    "code": "GRY",
+    "name": "Grays"
+  },
+  {
+    "code": "LAI",
+    "name": "Laindon"
+  },
+  {
+    "code": "LES",
+    "name": "Leigh-on-Sea"
+  },
+  {
+    "code": "LHS",
+    "name": "Limehouse"
+  },
+  {
+    "code": "LST",
+    "name": "London Liverpool Street"
+  },
+  {
+    "code": "OCK",
+    "name": "Ockendon"
+  },
+  {
+    "code": "PSE",
+    "name": "Pitsea"
+  },
+  {
+    "code": "PFL",
+    "name": "Purfleet"
+  },
+  {
+    "code": "RNM",
+    "name": "Rainham (Essex)"
+  },
+  {
+    "code": "SRY",
+    "name": "Shoeburyness"
+  },
+  {
+    "code": "SOC",
+    "name": "Southend Central"
+  },
+  {
+    "code": "SOE",
+    "name": "Southend East"
+  },
+  {
+    "code": "SRA",
+    "name": "Stratford (London)"
+  },
+  {
+    "code": "SFO",
+    "name": "Stanford-le-Hope"
+  },
+  {
+    "code": "TPB",
+    "name": "Thorpe Bay"
+  },
+  {
+    "code": "TIL",
+    "name": "Tilbury Town"
+  },
+  {
+    "code": "UPM",
+    "name": "Upminster"
+  },
+  {
+    "code": "WCF",
+    "name": "Westcliff"
+  },
+  {
+    "code": "WEH",
+    "name": "West Ham"
+  },
+  {
+    "code": "WHR",
+    "name": "West Horndon"
+  }
+];
